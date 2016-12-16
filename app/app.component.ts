f41818b776abe60c523d5e766b12be821df890b6
Sigmund Burgos <sigmund.utech@gmail.com>
2016-12-17 00:27:09 +0800

    Added gift list page.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>My New {{name}} App!</h1>`,
+  selector: 'gift-list',
+  template: `
+    <h1>{{title}}</h1>
+    <ul>
+      <li *ngFor="let gift of gifts">
+        {{gift.name}}
+      </li>
+    </ul>
+  `,
 })
-export class AppComponent  { name = 'Angular2'; }
+export class AppComponent {
+  title = 'Santa\'s List';
+
+  gifts = [
+    new Gift(1, 'Wallet', 'Father'),
+    new Gift(2, 'Apron', 'Mother'),
+    new Gift(3, 'Ball', 'Brother'),
+    new Gift(4, 'Bag', 'Sister'),
+    new Gift(5, 'Eyeglass', 'Grandpa'),
+  ];
+}
+
+export class Gift {
+  constructor(public id: number,
+    public name: string,
+    public receiver: string) { }
+}
