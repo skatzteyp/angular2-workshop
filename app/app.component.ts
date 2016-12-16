4fc8714ec79ebf4cb5f424176c47110b6e8a1a6a
Sigmund Burgos <sigmund.utech@gmail.com>
2016-12-16 14:45:34 +0800

    Added gift editor.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>My New {{name}} App!</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{gift.name}} Details</h2>
+    <div>ID: {{gift.id}}</div>
+    <div>Name: {{gift.name}} <input [(ngModel)]="gift.name" placeholder="Name" /></div>
+    <div>Receiver: {{gift.receiver}} <input [(ngModel)]="gift.receiver" placeholder="Receiver" /></div>
+  `,
 })
-export class AppComponent  { name = 'Angular2'; }
+export class AppComponent  {
+  title = 'Santa\'s List';
+
+  gift = { id: 1, name: 'Wallet', receiver: 'Father' };
+}
+
+export class Gift {
+  name: string;
+  receiver: string;
+}
